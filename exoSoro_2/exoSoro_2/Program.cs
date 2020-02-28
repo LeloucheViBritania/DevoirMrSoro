cc3a74c191f4a22ead24a571e95666fcc506b65d
Coulibaly Kafalo Alex <ckalexis20@gmail.com>
2020-02-28 15:40:50 +0000

    exo 2

---
+++ exoSoro_2/exoSoro_2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace exoSoro_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool etatGlace;
+            bool etatLiquide;
+            Console.WriteLine("Entrez une température : ");
+            int temp = Convert.ToInt32(Console.ReadLine());
+
+            if(temp >= 1 && temp <= 100)
+            {
+                etatGlace = false;
+                etatLiquide = true;
+                Console.WriteLine("L'eau est l'état Liquide");  
+            }
+
+            if(temp == 0 )
+            {
+                etatGlace = true;
+                etatLiquide = false;
+                Console.WriteLine("L'eau est l'état de Glace");
+            }
+
+            if(temp > 100)
+            {
+                etatGlace = false;
+                etatLiquide = false;
+                Console.WriteLine("L'eau est l'état de Vapeur");
+            }
+
+           
+            
+        }
+    }
+}
