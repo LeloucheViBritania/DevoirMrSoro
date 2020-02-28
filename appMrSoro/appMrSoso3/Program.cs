8fddcdbf88cf50fb30b9249cf7d3fb1df5156d74
LeloucheViBritania <kra.kobenan.olivier@gmail.com>
2020-02-28 18:15:27 +0000

    exo 3.1 fait v1

---
+++ appMrSoro/appMrSoso3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace appMrSoso3
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            double s = 0;
+            int n = 1;
+            Console.WriteLine("Entrer un nombre");
+            var numberUser = Console.ReadLine();
+            var numUserSur = Convert.ToDouble(numberUser);           
+            while(s <= numUserSur)
+            {
+                
+                s += (double)1 / n;
+                n += 1;
+
+            }
+            Console.WriteLine(n);
+            
+
+        }
+    }
+}
